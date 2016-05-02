1ac497d18dd7e55a77344401f6e7eb1e661ac7f4
Ryan Jones <ryandjones14@gmail.com>
2016-05-02 11:05:11 -0500

    calls api based on user input

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,24 @@ var router = express.Router();
 var request = require('request');
 var searchKey = process.env.ACTIVE_SEARCH_KEY;
 
+var city;
+var state;
+var activity;
+
 /* GET home page. */
 router.get('/', function(req, res, next){
   res.render('index', { title: 'Xplore'});
 })
 
+router.post('/trips', function(req, res, next){
+  city = req.body.city;
+  state = req.body.state;
+  activity = req.body.activity;
+  res.redirect('/trips');
+})
+
 router.get('/trips', function(req, res, next) {
-  var city = req.body.city;
-  var state = req.body.state;
-  var activity = req.body.activity;
+
 
   var queryString = `radius=50&city=${city}&state=${state}&current_page=1&per_page=10&sort=distance&topic=${activity}&exclude_children=true`;
 
