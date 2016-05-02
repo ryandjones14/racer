efd381edd3112c9e7effb2405a8a64a98398d9ee
Ryan Jones <ryandjones14@gmail.com>
2016-05-02 12:05:08 -0500

    changed climbing and hiking to running and tennis, changed trips to events

--- routes/index.js
+++ routes/index.js
@@ -12,31 +12,27 @@ router.get('/', function(req, res, next){
   res.render('index', { title: 'Xplore'});
 })
 
-router.post('/trips', function(req, res, next){
+router.post('/events', function(req, res, next){
   city = req.body.city;
   state = req.body.state;
   activity = req.body.activity;
-  res.redirect('/trips');
+  res.redirect('/events');
 })
 
-router.get('/trips', function(req, res, next) {
-
-
+router.get('/events', function(req, res, next) {
   var queryString = `radius=50&city=${city}&state=${state}&current_page=1&per_page=10&sort=distance&topic=${activity}&exclude_children=true`;
 
   var activeSearchURL = `http://api.amp.active.com/v2/search?${queryString}&api_key=${searchKey}`;
 
   request(activeSearchURL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // var data = JSON.parse(body);
-      // var activites = [];
-      // data.items.forEach(function(item) {
-      //   activites.push(item.family);
-      // });
-
-      // res.render('index', { title: 'Xplore', activites: activites});
-      console.log(activeSearchURL);
-      res.send(body);
+      var data = JSON.parse(body);
+      var activities = [];
+      data.results.forEach(function(result) {
+        activities.push(result);
+      });
+      res.render('events', { title: 'Xplore', activities: activities, activity: activity, city: city, state: state});
+      // res.send(activities);
     }
   })
 });
