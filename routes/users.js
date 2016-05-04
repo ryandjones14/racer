0febe83bfdf5dc810a0c4e3de3e05c9e6d10832b
Ryan Jones <ryandjones14@gmail.com>
2016-05-04 15:34:25 -0500

    users/all shows all users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var allUsers = [];
+  User.find({}, function(err, users) {
+    if (err) console.log(err);
+    console.log("USERS", users);
+    users.forEach(function(user){
+      allUsers.push(user);
+    })
+    console.log("ALL USERS", allUsers);
+    res.render('users/all', {title: 'racer', currentUser: req.session.currentUser, users: allUsers});
+  });
 });
 
 module.exports = router;
